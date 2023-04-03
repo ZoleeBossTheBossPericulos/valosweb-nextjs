fcec966006679a7387fab99d162583be278743b3
ZoleeBossTheBossPericulos <zfelhazi@yahoo.com>
2023-04-03 21:51:44 +0300

    feat: 4 different types of data accessing

---
+++ rendering-examples/helper/fetcher.ts
@@ -0,0 +1,8 @@
+export function apiFetcher<T>(url: string): Promise<T> {
+  return fetch(url).then((response) => {
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+    return response.json().then((data) => data as T);
+  });
+}
